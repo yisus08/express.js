ad079e43a2c98018f1f9cb0cbc707b5ac559a016
yisus08 <55464815+yisus08@users.noreply.github.com>
2019-10-01 18:56:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express');
+//index
+/*const express = require('express');
 const app = express();
+
 var ids=[];
 var personas={
 	nombre:"", edad:0,
@@ -15,16 +17,71 @@ app.get("/:id", (req, res) => {
     console.log(ids);
 });
 
-app.listen(3000, () =>{
-	console.log("Server is running...");
-});
 
-app.get("/:nombre"+"/:edad", (req, res) => {
+app.get("/:nombre""/:edad", (req, res) => {
 	personas.nombre=req.params.nombre;
 	personas.edad=req.params.edad;
 	res.send("hola mundo");
     console.log(personas);
 });
 
+app.listen(3000, () =>{
+	console.log("Server is running...");
+});*/
+
+//pokemon
+
+var express = require('express');
+var app = express();
+var pokedex = require('./pokedex.json');
+
+
+
+app.get("/", (req, res) => {
+	res.send('ingrese al pokedex');
+	//console.log(pokedex);
+
+});
+
+
+app.get("/pokedex/random", (req, res) => {
+	const num = Math.floor(Math.random()*(151-0)+0);
+	res.json(pokedex.pokemon[num]);
+
+});
+
+app.get("/pokedex/:id", (req, res) => {
+	const id = req.params.id;
+	if (id > 0 && id <= 151) {
+		res.json(pokedex.pokemon[req.params.id-1]);
+	}
+	else{
+		res.send("No hay ningun pokemon con ese id");
+	}
+	
+});
+
+app.get("/pokedex/:name", (req, res) => {
+	const name = req.params.name;
+	const pokemon = pokedex.pokemon.filter((item) => {return item.name == name});
+
+	res.json(pokemon);
+
+});
+
+
+
+app.get("/pokedex/img/:id", (req, res) => {
+	const img = pokedex.pokemon[req.params.id-1].img;
+	res.send("<img src='"+img+"'/>");
+
+});
+
+
+
+
+app.listen(3000, () =>{
+	console.log("Server is running...");
+});
 
 
